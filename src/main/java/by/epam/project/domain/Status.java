26816c83e322bc78c545dcba581e0e164cc9de91
Raman Kandratsenka <kondrat3nko@gmail.com>
2015-08-11 22:13:44 +0300

    добавил еще 3 таблицы

---
+++ src/main/java/by/epam/project/domain/Status.java
@@ -0,0 +1,31 @@
+package by.epam.project.domain;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "STATUS")
+public class Status {
+    @Id
+    @Column(name = "ID")
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer id;
+
+    @Column(name = "NAME")
+    private String name;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
