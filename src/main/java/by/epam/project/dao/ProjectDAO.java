b5409fd35549996f6608c33f6b989fddb5a88473
Maxim <test@alfagomel.by>
2015-08-11 15:54:20 +0300

    dashboard

---
+++ src/main/java/by/epam/project/dao/ProjectDAO.java
@@ -0,0 +1,14 @@
+package by.epam.project.dao;
+
+
+import by.epam.project.domain.Project;
+
+public interface ProjectDAO {
+
+    public void addProject(Project project);
+
+    public void addMember(Member member);
+
+
+
+}
---
+++ src/main/java/by/epam/project/dao/ProjectDAOImpl.java
@@ -0,0 +1,17 @@
+package by.epam.project.dao;
+
+import by.epam.project.domain.Project;
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class ProjectDAOImpl implements ProjectDAO {
+
+    @Autowired
+    private SessionFactory sessionFactory;
+
+    public void addProject(Project project) {
+        sessionFactory.getCurrentSession().save(project);
+    }
+}
