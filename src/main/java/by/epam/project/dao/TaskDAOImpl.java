884b6b79aba09f5c66327f1112eeda777dbf0466
Maxim <test@alfagomel.by>
2015-08-21 10:41:01 +0300

    Select Task work

--- src/main/java/by/epam/project/dao/TaskDAOImpl.java
+++ src/main/java/by/epam/project/dao/TaskDAOImpl.java
@@ -6,7 +6,6 @@ import org.hibernate.SessionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @Repository
@@ -22,12 +21,18 @@ public class TaskDAOImpl implements TaskDAO {
 
     @Override
     public List<Task> getTaskLogin (String login) {
-        String stringQuery = "select distinct t from Task t " +
+        /*String stringQuery = "select distinct t from Task t " +
                 "left join t.project p " +
                 "left join p.employees e " +
+                "where e.login=:login";*/
+        String stringQuery = "select distinct t from Assigment a " +
+                "inner join a.task t " +
+                "inner join a.member m " +
+                "inner join m.employee e " +
                 "where e.login=:login";
         Query query = sessionFactory.getCurrentSession().createQuery(stringQuery);
-        query.setParameter("login",login);
+        query.setParameter("login", login);
+        System.out.println("Current login is " + login);
         List<Task> tasks = query.list();
         return tasks;
     }
