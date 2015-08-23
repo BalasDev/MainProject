f52a962cf0b50dce20fb960e4cbf495e92a0829b
Raman Kandratsenka <kondrat3nko@gmail.com>
2015-08-24 00:00:50 +0300

    для подгрузки файла

--- src/main/java/by/epam/project/domain/FileUploadForm.java
+++ src/main/java/by/epam/project/domain/FileUploadForm.java
@@ -1,6 +1,19 @@
 package by.epam.project.domain;
 
 
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.List;
+
 public class FileUploadForm {
+    public List<MultipartFile> getFiles() {
+        return files;
+    }
+
+    public void setFiles(List<MultipartFile> files) {
+        this.files = files;
+    }
+
+    private List<MultipartFile> files;
 
 }
